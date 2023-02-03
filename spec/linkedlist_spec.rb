e42f35649a7c9acef224a2ffbe1188b0265743d7
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-02-02 20:26:41 -0700

    test: add test for linked list

--- spec/linkedlist_spec.rb
+++ spec/linkedlist_spec.rb
@@ -1,2 +1,9 @@
-"./lib/linked_list"
+require "./lib/linkedlist"
 
+RSpec.describe LinkedList do
+  it 'it exists' do
+    list = LinkedList.new
+
+    expect(list).to be_instance_of(LinkedList)
+  end
+end
