6c203c671cd5c7fff48fff60108de9b8110b63d8
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-02-02 20:39:43 -0700

    test: add test for new dta append

--- spec/linkedlist_spec.rb
+++ spec/linkedlist_spec.rb
@@ -1,13 +1,20 @@
+require "./lib/node"
 require "./lib/linkedlist"
 
 RSpec.describe LinkedList do
-  it 'it exists, head is nil' do
+  it 'exists, head is nil' do
     list = LinkedList.new
 
     expect(list).to be_instance_of(LinkedList)
     expect(list.head).to eq(nil)
   end
-  
+
+  it 'adds a new piece of data to list #append' do
+    list = LinkedList.new
+
+    expect(list.append("doop")).to eq("doop")
+  end
+
 
 
 end
