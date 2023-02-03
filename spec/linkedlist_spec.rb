5ccb0e1b50ef36617ec2788495617cb4ef492354
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-02-02 20:31:23 -0700

    test: add list head

--- spec/linkedlist_spec.rb
+++ spec/linkedlist_spec.rb
@@ -1,9 +1,13 @@
 require "./lib/linkedlist"
 
 RSpec.describe LinkedList do
-  it 'it exists' do
+  it 'it exists, head is nil' do
     list = LinkedList.new
 
     expect(list).to be_instance_of(LinkedList)
+    expect(list.head).to eq(nil)
   end
+  
+
+
 end
