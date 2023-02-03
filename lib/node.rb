6b9deb02cfddea82b69821b80ef4521c5c47c812
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-02-02 21:14:46 -0700

    refactor: fix next node def to make it default

--- lib/node.rb
+++ lib/node.rb
@@ -1,15 +1,12 @@
 class Node
 
-  def initialize(sound)
+  attr_accessor :sound, :next_node
+  def initialize(sound, next_node = nil)
     @sound = sound
   end
 
   def data
     @sound
-  end
-
-  def next_node
-    nil
   end
 end
 
