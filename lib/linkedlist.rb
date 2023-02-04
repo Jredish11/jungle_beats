3d98f817d4b3310f2f66fccde0424e82e1d6f24e
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-02-04 10:01:53 -0700

    refactor: fixed count def, added until loop

--- lib/linkedlist.rb
+++ lib/linkedlist.rb
@@ -1,6 +1,6 @@
 class LinkedList
 
-  attr_accessor :head, :data, :sound
+  attr_accessor :head, :data
   def initialize
     @head = nil
   end
@@ -9,14 +9,14 @@ class LinkedList
     @head = Node.new(data) #creat node object
   end
 
-   
-    
-    
-  
-
-
   def count
-    @count = 1
+    count = 0    #count starts at 0
+    current_node = @head   #current_node = ^ Node.new(data)
+    until current_node.nil?  #loop until current_node = nil
+      count += 1   #count each current node
+      current_node = current_node.next_node  #current_node equals nil when nextnode is called on it. ends loop and count.
+    end
+    count  #prints the count of nodes 
   end
 
   def to_string
