4368bc72c71fd097c50cfba5a9fae8a3b08a4fc4
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-02-06 19:22:16 -0700

    refactor: fix names for local variable

--- lib/linkedlist.rb
+++ lib/linkedlist.rb
@@ -1,25 +1,54 @@
 class LinkedList
 
-  attr_accessor :head, :data
+  attr_accessor :head, :data, :next_node
   def initialize
     @head = nil
   end
 
-  def append(data)
-    @head = Node.new(data) #creat node object
-  end
+
+
+  def append(data) #adding node to end of list
+    if @head.nil? ##if nothing is here i need to start a node. if head is nil create new node
+    head_node = Node.new(data)
+    @head = head_node
+    else
+      new_node = Node.new(data)
+      current_node = @head
+      until current_node.next_node.nil?
+        current_node = current_node.next_node
+      end
+      current_node.next_node = new_node
+    end
+ end
 
   def count
-    count = 0    #count starts at 0
-    current_node = @head   #current_node = ^ Node.new(data)
-    until current_node.nil?  #loop until current_node = nil
-      count += 1   #count each current node
-      current_node = current_node.next_node  #current_node equals nil when nextnode is called on it. ends loop and count.
+    count = 1
+    if @head.nil?
+      0
+    else 
+      current_node = @head
+      until current_node.next_node.nil?
+        current_node = current_node.next_node
+        count += 1
+      end
+      count
     end
-    count  #prints the count of nodes 
   end
 
   def to_string
-    @head.data
+    results_string = ""
+    if @head.nil?
+      results_string
+    else 
+      results_string = @head.data
+      current_node = @head
+      until current_node.next_node.nil?
+        current_node = current_node.next_node
+        results_string = results_string + " " + current_node.data
+      end
+      results_string
+    end
   end
 end
+
+
