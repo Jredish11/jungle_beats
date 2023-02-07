04831a123e86add55a3021b3e3ca1e95c7cfc94e
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-02-07 11:40:09 -0700

    test: add test for insert method

--- spec/linkedlist_spec.rb
+++ spec/linkedlist_spec.rb
@@ -12,12 +12,13 @@ RSpec.describe LinkedList do
   it '#append' do
     list = LinkedList.new
     list.append("doop")
+    
 
     expect(list).to be_instance_of(LinkedList)
     expect(list.head).to be_instance_of(Node)
   
     expect(list.head.data).to eq("doop")
-    expect(list.head.next_node).to eq(nil)
+    expect(list.next_node).to eq(nil)
   end
 
   it "#count" do
@@ -25,16 +26,64 @@ RSpec.describe LinkedList do
     list.append("doop")
 
     expect(list.count).to eq(1)
+    
   end
   
   it '#to_string' do 
   list = LinkedList.new
   list.append("doop")
-
+  
+  
   expect(list.to_string).to eq("doop")
   end
 
+  it "append new node into list" do
+    list = LinkedList.new
+    list.append("doop")
+    list.append("deep")
+    
 
+    expect(list.head.data).to eq("doop")
+    expect(list.head).to be_instance_of(Node)
+    expect(list.count).to eq(2)
+    expect(list.to_string).to eq("doop deep")
+  end
 
+  it "append new node 'plop'" do
+    list = LinkedList.new
+    list.append("plop")
+  
+    expect(list.head.data).to eq("plop")
+    expect(list.to_string).to eq("plop")
+  end
+  
+  it 'append new node suu ' do
+    list = LinkedList.new
+    list.append("suu")
+
+    expect(list.head.data).to eq("suu")
+  end
+
+  it 'prepend method' do
+    list = LinkedList.new
+    list.append("plop")
+    list.append("suu")
+    list.prepend("dop")
+    
+
+    expect(list.head.data).to eq("dop")
+    expect(list.to_string).to eq("dop plop suu")
+    expect(list.count).to eq (3)
+  end
+
+  it 'tests insert method' do
+    list = LinkedList.new
+    list.append("plop")
+    list.append("suu")
+    list.prepend("dop")
+    list.insert(1, "woo")
+
+    expect(list.to_string).to eq("dop woo plop suu")
+  end
 end
 
